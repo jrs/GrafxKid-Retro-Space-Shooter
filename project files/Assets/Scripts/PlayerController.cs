7a4efef8797e931c51f05fbd4b43e67f5fb839a8
Justin Scott <hello@jrs.dev>
2022-10-12 13:38:39 -0700

    changes to player movement

--- project files/Assets/Scripts/PlayerController.cs
+++ project files/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float _moveSpeed = 5;
+    [SerializeField] float _xRange = 8;
+    [SerializeField] float _yRange = 4;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,10 +16,35 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Movement();
+    }
+
+    private void Movement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector2.right * horizontalInput * 5 * Time.deltaTime);
-        transform.Translate(Vector2.up * verticalInput * 5 * Time.deltaTime);
+        transform.Translate(Vector2.right * horizontalInput * _moveSpeed * Time.deltaTime);
+        transform.Translate(Vector2.up * verticalInput * _moveSpeed * Time.deltaTime);
+
+        if(transform.position.x < -_xRange)
+        {
+            transform.position = new Vector2(-_xRange, transform.position.y);
+        }
+
+        if(transform.position.x > _xRange)
+        {
+            transform.position = new Vector2(_xRange, transform.position.y);
+        }
+
+        if(transform.position.y < -_yRange)
+        {
+            transform.position = new Vector2(transform.position.x, -_yRange);
+        }
+
+        if(transform.position.y > _yRange)
+        {
+            transform.position = new Vector2(transform.position.x, _yRange);
+        }
     }
 }
