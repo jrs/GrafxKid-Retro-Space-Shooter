d538c2bbe7e5bea5e69984cfc10e89bb9e391e46
Justin Scott <hello@jrs.dev>
2022-11-01 15:45:56 -0700

    update

--- project files/Assets/Scripts/Player.cs
+++ project files/Assets/Scripts/Player.cs
@@ -8,69 +8,83 @@ public class Player : MonoBehaviour
     public Animator PlayerBooter;
     public GameObject ProjectilePrefab;
     public GameObject ExplosionPrefab;
-
+    public bool PlayerCanMove;
     [SerializeField] float _moveSpeed = 5;
     [SerializeField] int _playerHealth = 10;
-    
+
+        
     private float _xRange = 8;
     private float _yRange = 4;
     private Animator _playerAnim;
+    private Vector2 target;
+    private Vector2 position;
 
     // Start is called before the first frame update
     void Start()
     {
         _playerAnim = GetComponent<Animator>();
+        target = new Vector2(0.0f, 0.0f);
+        position = gameObject.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float step = 1f * Time.deltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, target, step);
+        if(transform.position.y == 0)
+        {
+            PlayerCanMove = true;
+        }
         Movement();
         FireShot();
-    }
+    } 
 
     private void Movement()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        float verticalInput = Input.GetAxisRaw("Vertical");
-        Vector2 direction = new Vector2(horizontalInput, verticalInput);
+        if(PlayerCanMove)
+        {
+            float horizontalInput = Input.GetAxisRaw("Horizontal");
+            float verticalInput = Input.GetAxisRaw("Vertical");
+            Vector2 direction = new Vector2(horizontalInput, verticalInput);
 
-        transform.Translate(direction * _moveSpeed * Time.deltaTime);
+            transform.Translate(direction * _moveSpeed * Time.deltaTime);
+            
+            if(transform.position.x < -_xRange)
+            {
+                transform.position = new Vector2(-_xRange, transform.position.y);
+            }
 
-        if(transform.position.x < -_xRange)
-        {
-            transform.position = new Vector2(-_xRange, transform.position.y);
-        }
+            if(transform.position.x > _xRange)
+            {
+                transform.position = new Vector2(_xRange, transform.position.y);
+            }
 
-        if(transform.position.x > _xRange)
-        {
-            transform.position = new Vector2(_xRange, transform.position.y);
-        }
+            if(transform.position.y < -_yRange)
+            {
+                transform.position = new Vector2(transform.position.x, -_yRange);
+            }
 
-        if(transform.position.y < -_yRange)
-        {
-            transform.position = new Vector2(transform.position.x, -_yRange);
-        }
+            if(transform.position.y > _yRange)
+            {
+                transform.position = new Vector2(transform.position.x, _yRange);
+            }
 
-        if(transform.position.y > _yRange)
-        {
-            transform.position = new Vector2(transform.position.x, _yRange);
-        }
-
-        if (horizontalInput > 0)
-        {
-            _playerAnim.SetInteger("TurnDirection", 1);
-            PlayerBooter.SetInteger("BoostDirection", 1);
-        }
-        else if (horizontalInput < 0)
-        {
-            _playerAnim.SetInteger("TurnDirection", -1);
-            PlayerBooter.SetInteger("BoostDirection", -1);
-        }
-        else
-        {
-            _playerAnim.SetInteger("TurnDirection", 0);
-            PlayerBooter.SetInteger("BoostDirection", 0);
+            if (horizontalInput > 0)
+            {
+                _playerAnim.SetInteger("TurnDirection", 1);
+                PlayerBooter.SetInteger("BoostDirection", 1);
+            }
+            else if (horizontalInput < 0)
+            {
+                _playerAnim.SetInteger("TurnDirection", -1);
+                PlayerBooter.SetInteger("BoostDirection", -1);
+            }
+            else
+            {
+                _playerAnim.SetInteger("TurnDirection", 0);
+                PlayerBooter.SetInteger("BoostDirection", 0);
+            }
         }
 
     }
@@ -102,5 +116,13 @@ public class Player : MonoBehaviour
             Instantiate(ExplosionPrefab, transform.position, ExplosionPrefab.transform.rotation);
             UpdatePlayerHealth(1);
         }
+
+        if(other.CompareTag("Enemy Projectile"))
+        {
+            Instantiate(ExplosionPrefab, transform.position, ExplosionPrefab.transform.rotation);
+            UpdatePlayerHealth(1);
+            Destroy(other.gameObject);
+        }
     }
+
 }
